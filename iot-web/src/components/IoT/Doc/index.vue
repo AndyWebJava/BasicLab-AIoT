aa8c6fa05f9734843daa5c390995f48f1dff3961
seven.zhang <andywebjava@163.com>
2024-07-08 11:36:28 +0800

    bug:修正mapper映射问题和前端映射问题

---
+++ iot-web/src/components/IoT/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IoTDoc',
+  data() {
+    return {
+      url: 'http://doc.IoT.iot.com'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
---
+++ iot-web/src/components/IoT/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IoTGit',
+  data() {
+    return {
+      url: 'https://github.com/AndyWebJava/basiclab-iot.git'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
